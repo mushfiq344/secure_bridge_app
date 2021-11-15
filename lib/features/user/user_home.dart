f3aa95bf267d289b53ccae33fb9ef84c1fcfd8c7
DESKTOP-9U662K2\Mushfiqur <rahmanmushfiq94@gmail.com>
2021-11-16 00:17:00 +0600

    overflow bug fixed

--- lib/features/user/user_home.dart
+++ lib/features/user/user_home.dart
@@ -74,162 +74,164 @@ class _UserHomeState extends State<UserHome> {
       ),
       body: Container(
         alignment: Alignment.center,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-              horizontal: kMargin16, vertical: kMargin16),
-          child: Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    color: kPinkBackground,
-                    borderRadius: BorderRadius.all(Radius.circular(29))),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: kMargin48, horizontal: kMargin32),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "AWARENESS TOGETHER",
-                        style: TextStyle(
-                            fontSize: 36,
-                            color: kPurpleColor,
-                            fontWeight: FontWeight.w400),
-                      ),
-                      Text(
-                        "Find what fascinates you as you explore these habit courses.",
-                        style: TextStyle(
-                            fontSize: 10,
-                            color: kPurpleColor,
-                            fontWeight: FontWeight.w700),
-                      )
-                    ],
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: kMargin16, vertical: kMargin16),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: kPinkBackground,
+                      borderRadius: BorderRadius.all(Radius.circular(29))),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: kMargin48, horizontal: kMargin32),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "AWARENESS TOGETHER",
+                          style: TextStyle(
+                              fontSize: 36,
+                              color: kPurpleColor,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        Text(
+                          "Find what fascinates you as you explore these habit courses.",
+                          style: TextStyle(
+                              fontSize: 10,
+                              color: kPurpleColor,
+                              fontWeight: FontWeight.w700),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: kMargin20,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: AspectRatio(
-                      aspectRatio: 1 / 1,
+                SizedBox(
+                  height: kMargin20,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: AspectRatio(
+                        aspectRatio: 1 / 1,
+                        child: Card(
+                          color: kPurpleBackGround,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(26)),
+                          child: Image(
+                            image: AssetImage(kHomeWhiteIconPath),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
                       child: Card(
                         color: kPurpleBackGround,
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(26)),
-                        child: Image(
-                          image: AssetImage(kHomeWhiteIconPath),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Card(
-                      color: kPurpleBackGround,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(26)),
-                      child: AspectRatio(
-                        aspectRatio: 1 / 1,
-                        child: Card(
-                          color: kPurpleBackGround,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(26)),
-                          child: Image(
-                            image: AssetImage(kMultipleUsersIconPath),
+                        child: AspectRatio(
+                          aspectRatio: 1 / 1,
+                          child: Card(
+                            color: kPurpleBackGround,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(26)),
+                            child: Image(
+                              image: AssetImage(kMultipleUsersIconPath),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Card(
-                      color: kPurpleBackGround,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(26)),
-                      child: AspectRatio(
-                        aspectRatio: 1 / 1,
-                        child: Card(
-                          color: kPurpleBackGround,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(26)),
-                          child: Image(
-                            image: AssetImage(kAidIconPath),
+                    Expanded(
+                      flex: 1,
+                      child: Card(
+                        color: kPurpleBackGround,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(26)),
+                        child: AspectRatio(
+                          aspectRatio: 1 / 1,
+                          child: Card(
+                            color: kPurpleBackGround,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(26)),
+                            child: Image(
+                              image: AssetImage(kAidIconPath),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Card(
-                      color: kPurpleBackGround,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(26)),
-                      child: AspectRatio(
-                        aspectRatio: 1 / 1,
-                        child: Card(
-                          color: kPurpleBackGround,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(26)),
-                          child: Image(
-                            image: AssetImage(kPizzaIconPath),
+                    Expanded(
+                      flex: 1,
+                      child: Card(
+                        color: kPurpleBackGround,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(26)),
+                        child: AspectRatio(
+                          aspectRatio: 1 / 1,
+                          child: Card(
+                            color: kPurpleBackGround,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(26)),
+                            child: Image(
+                              image: AssetImage(kPizzaIconPath),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: kMargin20,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: FormBuilderDropdown(
-                      name: 'gender',
-                      decoration: customInputDecoration("Popular"),
-                      // initialValue: 'Male',
-                      allowClear: true,
+                  ],
+                ),
+                SizedBox(
+                  height: kMargin20,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: FormBuilderDropdown(
+                        name: 'gender',
+                        decoration: customInputDecoration("Popular"),
+                        // initialValue: 'Male',
+                        allowClear: true,
 
-                      validator: FormBuilderValidators.compose(
-                          [FormBuilderValidators.required(context)]),
-                      items: ["male", "female", "others"]
-                          .map((gender) => DropdownMenuItem(
-                                value: gender,
-                                child: Text('$gender'),
-                              ))
-                          .toList(),
+                        validator: FormBuilderValidators.compose(
+                            [FormBuilderValidators.required(context)]),
+                        items: ["male", "female", "others"]
+                            .map((gender) => DropdownMenuItem(
+                                  value: gender,
+                                  child: Text('$gender'),
+                                ))
+                            .toList(),
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: FormBuilderDropdown(
-                      name: 'gender',
-                      decoration: customInputDecoration("Filters"),
-                      // initialValue: 'Male',
-                      allowClear: true,
+                    Expanded(
+                      flex: 1,
+                      child: FormBuilderDropdown(
+                        name: 'gender',
+                        decoration: customInputDecoration("Filters"),
+                        // initialValue: 'Male',
+                        allowClear: true,
 
-                      validator: FormBuilderValidators.compose(
-                          [FormBuilderValidators.required(context)]),
-                      items: ["male", "female", "others"]
-                          .map((gender) => DropdownMenuItem(
-                                value: gender,
-                                child: Text('$gender'),
-                              ))
-                          .toList(),
-                    ),
-                  )
-                ],
-              ),
-              _buildOpportunityList(context)
-            ],
+                        validator: FormBuilderValidators.compose(
+                            [FormBuilderValidators.required(context)]),
+                        items: ["male", "female", "others"]
+                            .map((gender) => DropdownMenuItem(
+                                  value: gender,
+                                  child: Text('$gender'),
+                                ))
+                            .toList(),
+                      ),
+                    )
+                  ],
+                ),
+                _buildOpportunityList(context)
+              ],
+            ),
           ),
         ),
       ),
